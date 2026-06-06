63885a4ac0a4ebe42b22e3147c9576474e2c87fe
ebenezer <ebenezer6374@gmail.com>
2026-06-06 10:27:05 +0530

    Third Commit

---
+++ Divisors.py
@@ -0,0 +1,20 @@
+no = 10
+i = 1
+count = 0
+while i <=no:
+    if no % i ==0:
+        print (i)
+        count +=1
+    i+=1
+print (count)
+
+
+#no  =10
+#divisors = []
+#count = 0
+#for i in range (1, no +1):
+#    if no % i ==0:
+#        divisors.append(i)
+#        count  +=1
+#print(divisors)
+#print (count)
---
+++ Multiples of 3 & 5.py
@@ -0,0 +1,16 @@
+no =1
+while no <=100:
+    if no % 3 ==0 and no % 5 ==0:
+        print (no)
+    no +=1
+
+#no =1
+#while no <=100:
+#    if no % 3 ==0 or no % 5 ==0:
+#        print (no , end = " ")
+#    no +=1
+
+#no =100
+#for i in range(1, no+1):
+#    if   i % 3 ==0 or i % 5 ==0:
+#        print (i)
